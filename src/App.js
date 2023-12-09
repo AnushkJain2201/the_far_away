921177ffc8f293446ffcfdbc077999eaa46055df
Anushk Jain <anushk2201@gmail.com>
2023-12-09 09:37:52 +0530

    Basic Layout Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 function App() {
   return (
-    <div>
-      <h1>Hello React</h1>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
 export default App;
+
+const Logo = () => {
+  return <h1>🌴 The Far Away 🎒</h1>
+
+}
+
+const Form = () => {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+const PackingList = () => {
+  return (
+    <div className="list">
+      List
+    </div>
+  );
+}
+
+const Stats = () => {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
