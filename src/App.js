03c1cbc3eda0e10ae995aca2c8fa6ecd915002a5
Anushk Jain <anushk2201@gmail.com>
2023-12-09 12:23:39 +0530

    Controlled Elements Done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -18,15 +20,52 @@ export default App;
 
 const Logo = () => {
   return <h1>🌴 The Far Away 🎒</h1>
-
 }
 
 const Form = () => {
+  // This state is for the input field and then pass that state in the value attribute of that input field
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  const handleSubmit = (e) => {
+    // When we submit a form there will be a brief flash that happens due to the default behaviour of HTML, so to prevent this behaviour, we write the preventDefault method. But with this all the effects of HTML will be gone and we have to write the logic ourself.
+    e.preventDefault();
+
+    // If there is no description, we will not submit the form
+    if(!description) return;
+
+    // Creating an object for newly submitted value
+    const newItem = {
+      description, quantity, packed: false, id: Date.now()
+    };
+
+    // After submiting the form successfully, we will insert the default value in the input fields.
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
 
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+
+      {/* Here, also controlling element */}
+      {/* The value of the e.target.value is always string, so in order to convert it into the number we used the Number function */}
+      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+        {/* Here we, are using an Array.from method to create an Array of 20 elements
+        The first argument contains an object of length property, the second argument contain an function with first argument as element and second argument as index and we are only interested in index only here */}
+        {Array.from({length: 20}, (_, i) => i + 1).map(
+          (num) => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          )
+        )}
+      </select>
+      {/* Here, we have to connect the state with the input field by handling the onChange event, and setting that state to the value of input field everytime it is changed */}
+      <input type="text" placeholder="Item...." value={description} onChange={(e) => setDescription(e.target.value)}/>
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -36,7 +75,7 @@ const PackingList = () => {
 
       <ul>
         {initialItems.map(item => 
-          (<Item item={item}/>
+          (<Item item={item} key={item.id}/>
         ))}
       </ul>
     </div>
